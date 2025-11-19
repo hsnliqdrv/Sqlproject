5204380862d0d34080d7aa89c8fc76ff753562f4
Həsənəli Qədirov <61022317+hsnliqdrv@users.noreply.github.com>
2025-11-19 17:06:08 +0400

    Create subquery.sql

---
+++ subquery.sql
@@ -0,0 +1,3 @@
+2. Find all Cars where the price is the highest price inside their own Orders. Show the Cars name and the price.
+3. Show all Orders that have at least one Cars with status = 'Discontinued' or 'Inactive'. We need this list to plan new items.
+4. Find Cars where the price is higher than the price of all other Cars in the same Orders. These are top items in each group.
